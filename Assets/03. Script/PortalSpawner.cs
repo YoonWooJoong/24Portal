41adaf91694ac1ef1e69796a68b729f0a0a14847
min4937 <pmy1504@naver.com>
2025-03-12 20:45:53 +0900

    [feat:MY]렌더링 텍스쳐추가, 테스트 플레이어가 추가

--- Assets/03. Script/PortalSpawner.cs
+++ Assets/03. Script/PortalSpawner.cs
@@ -4,39 +4,54 @@ using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
-{   
-    public GameObject portalPrefab;   
-    public float maxSpawnDistance = 100f;    
+{    
+    public GameObject portalAPrefab; 
+    public GameObject portalBPrefab; 
+    public float maxSpawnDistance = 100f;
+
     private GameObject portalA;
     private GameObject portalB;
 
     void Update()
-    {        
+    {
         if (Input.GetMouseButtonDown(0))
         {
-            SpawnPortal(ref portalA);
-        }                
+            SpawnPortal(ref portalA, portalAPrefab);
+        }
+
         if (Input.GetMouseButtonDown(1))
         {
-            SpawnPortal(ref portalB);
+            SpawnPortal(ref portalB, portalBPrefab);
         }
     }
 
-    void SpawnPortal(ref GameObject portal)    {
-        
+    void SpawnPortal(ref GameObject portal, GameObject prefab)
+    {
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit;
-        
+
         if (Physics.Raycast(ray, out hit, maxSpawnDistance))
-        {            
+        {
             if (portal != null)
             {
                 Destroy(portal);
             }
-                        
-            portal = Instantiate(portalPrefab, hit.point, Quaternion.identity);            
-            portal.transform.rotation = Quaternion.LookRotation(hit.normal);
-                       
+
+
+            float zOffset = 0.3f;
+            float xOffset = 0.4f;
+            Vector3 spawnPosition = hit.point + new Vector3(xOffset, 0f, zOffset);
+
+            // 포탈 생성 및 변수 할당
+            GameObject newPortal = Instantiate(prefab, hit.point, Quaternion.identity);
+            portal = newPortal;
+
+            Portal portalScript = newPortal.GetComponent<Portal>();
+
+            Quaternion additionalRotation = Quaternion.Euler(90f, 0f, 0f);
+            portal.transform.rotation = Quaternion.LookRotation(hit.normal) * additionalRotation;
+
+            // 포탈이 모두 생성되었으면 연결
             if (portalA != null && portalB != null)
             {
                 ConnectPortals();
@@ -49,10 +64,13 @@ public class NewBehaviourScript : MonoBehaviour
     }
 
     void ConnectPortals()
-    {        
+    {
+        // 포탈 A와 B가 모두 생성되었는지 확인
+        if (portalA == null || portalB == null) return;
+
         Portal portalAScript = portalA.GetComponent<Portal>();
         Portal portalBScript = portalB.GetComponent<Portal>();
-                
+
         if (portalAScript == null)
         {
             portalAScript = portalA.AddComponent<Portal>();
@@ -61,11 +79,15 @@ public class NewBehaviourScript : MonoBehaviour
         {
             portalBScript = portalB.AddComponent<Portal>();
         }
-               
-        portalAScript.otherPortal = portalB.transform;
-        portalBScript.otherPortal = portalA.transform;
 
-        Debug.Log("포탈 A와 B가 연결되었습니다.");
+        // portalA와 portalB가 null이 아닌지 확인
+        if (portalAScript != null && portalBScript != null)
+        {
+            portalAScript.otherPortal = portalB.transform;
+            portalBScript.otherPortal = portalA.transform;
+
+            Debug.Log("포탈 A와 B가 연결되었습니다.");
+        }
     }
 }
 
