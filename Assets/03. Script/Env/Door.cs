e97f0cc8b89b9a36078d4728d8275c849a3afd35
Lee Sanghun <hun1387s@gmail.com>
2025-03-13 10:59:12 +0900

    [Feat: SH] Door 애니메이션 및 파라미터 노출 적용

---
+++ Assets/03. Script/Env/Door.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    public Animator anim;
+    public bool isOpen;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        anim.SetBool("IsOpen", isOpen);
+    }
+
+
+}
