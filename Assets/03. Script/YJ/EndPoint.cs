d16feab450499563d389e7570ed94c9a6d9098cd
YoonWooJoong <dbsdnwnd1126@gmail.com>
2025-03-14 17:44:24 +0900

    [Feat : WJ] EndPoint 접촉시 2초후 넘어가게 설정

--- Assets/03. Script/YJ/EndPoint.cs
+++ Assets/03. Script/YJ/EndPoint.cs
@@ -1,16 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class EndPoint : MonoBehaviour
 {
+    private Coroutine nowCo;
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            GameManager.Instance.stageChanger.NextScene();
-            PlayerPrefs.SetInt("Level", SceneManager.GetActiveScene().buildIndex+1);
+            nowCo = StartCoroutine(DelayNextScene());
+
         }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            if (nowCo != null)
+            {
+                StopCoroutine(nowCo);
+            }
+        }
+    }
+
+    IEnumerator DelayNextScene()
+    {
+        yield return new WaitForSeconds(2);
+        GameManager.Instance.stageChanger.NextScene();
+        PlayerPrefs.SetInt("Level", SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
