dd3ccf99ca800b51d5a7118f21d462b0192c11e4
Hwanyi <wksgo123@knu.ac.kr>
2025-03-18 01:13:48 +0900

    [Feat : TH} 체력 구현 / 이펙트는 아직 미구현

--- Assets/03. Script/Player/Player.cs
+++ Assets/03. Script/Player/Player.cs
@@ -1,10 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
     public PlayerController controller;
+    public float curHP;
+    public float maxHp;
+    public float recoveryRate;
+
+    public float currentHitTime;
+    public float hitGap;
+
+    public Image hitImage;
+
+    private void Awake()
+    {
+        curHP = maxHp;
+        currentHitTime = Time.time;
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -12,4 +27,37 @@ public class Player : MonoBehaviour
         controller = GetComponent<PlayerController>();
     }
 
+    private void Update()
+    {
+        if (hitGap < Time.time - currentHitTime)
+        {
+            if(curHP <= maxHp)
+            {
+                curHP += recoveryRate * Time.deltaTime;
+            }
+            else
+            {
+                curHP = maxHp;
+            }
+        }
+    }
+    
+    public void TakeDamage(float damage)
+    {
+        curHP -= damage;
+        if (curHP <= 0)
+        {
+            Die();
+        }
+    }
+
+    public void Die()
+    {
+        //죽었을 때 스테이지 초기화 등...
+    }
+
+    private void HitImageColorChange()
+    {
+        hitImage.color = new Color(1, 0, 0, (1 - curHP / maxHp) * 0.5f);
+    }
 }
